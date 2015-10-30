e6d2c33ad97141e55794cd35526a67db1717e5ee
riansyahrial <riansyahrial@gmail.com>
2015-10-30 16:33:49 +0700

    praktikum vektor 1.1

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,14 @@
+
+
+public class Vector3D
+{
+    double x;
+    double y;
+    double z;
+    
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
