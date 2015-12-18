0a6b3cfe37318cce6d3ea52ca705f51dbf43916d
rian syahrial <riansyahrial@gmail.com>
2015-12-18 16:02:04 +0700

    TEST AWAL

--- 27-chatupperthread/22-Chat-Tugas-Solved/Client.java
+++ 27-chatupperthread/22-Chat-Tugas-Solved/Client.java
@@ -1,48 +1,119 @@
-import java.net.Socket;
-import java.net.UnknownHostException;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.BufferedReader;
-
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.io.BufferedWriter;
-
-import java.util.Scanner;
-
-public class Client {
-    public void chat() 
-                throws UnknownHostException, IOException {
-        Socket socket = new Socket("localhost", 33333);
-        
-        try {
-            // Ketik
-            Scanner keyboard = new Scanner(System.in);
-            System.out.print("Pesan: ");
-            String ketikanSatuBaris = keyboard.nextLine();
-                    
-            // Tulis ke socket
-            Writer keluaranWriter = new OutputStreamWriter(socket.getOutputStream()); 
-            BufferedWriter keluaranBuff = new BufferedWriter(keluaranWriter);
-            keluaranBuff.write(ketikanSatuBaris);
-            keluaranBuff.write("\n");
-            keluaranBuff.flush();
-                
-            // Baca dari Server
-            System.out.print("Dari server: ");
-            Reader masukan = new InputStreamReader(socket.getInputStream()); 
-            BufferedReader masukanBuff = new BufferedReader(masukan);
-            String baris = masukanBuff.readLine();
-            System.out.println(baris);                
-        }
-        catch(IOException salah) {
-            System.out.println(salah);
-        }
-        finally {
-            if (socket != null)
-            socket.close();
-        }
-    }
-}
+import java.net.Socket; 
+2 import java.net.UnknownHostException; 
+3 
+ 
+4 import java.io.IOException; 
+5 import java.io.InputStreamReader; 
+6 import java.io.Reader; 
+7 import java.io.BufferedReader; 
+8 
+ 
+9 import java.io.OutputStreamWriter; 
+10 import java.io.Writer; 
+11 import java.io.BufferedWriter; 
+12 
+ 
+13 import java.util.Scanner; 
+14 
+ 
+15 public class Client { 
+16     public void chat()  
+17                 throws UnknownHostException, IOException { 
+18         Socket socket = new Socket("localhost", 33333); 
+19          
+20         try { 
+21             // Ketik 
+22             Scanner keyboard = new Scanner(System.in); 
+23             System.out.print("Pesan: "); 
+24             String ketikanSatuBaris = keyboard.nextLine(); 
+25                      
+26             // Tulis ke socket 
+27             Writer keluaranWriter = new OutputStreamWriter(socket.getOutputStream());  
+28             BufferedWriter keluaranBuff = new BufferedWriter(keluaranWriter); 
+29             keluaranBuff.write(ketikanSatuBaris); 
+30             keluaranBuff.write("\n"); 
+31             keluaranBuff.flush(); 
+32                  
+33             // Baca dari Server 
+34             System.out.print("Dari server: "); 
+35             Reader masukan = new InputStreamReader(socket.getInputStream());  
+36             BufferedReader masukanBuff = new BufferedReader(masukan); 
+37             String baris = masukanBuff.readLine(); 
+38             System.out.println(baris);              
+39              
+40             // Mengupperkan apa yang dituliskan oleh server 
+41             String diupperkan; 
+42             diupperkan = baris.toUpperCase();             
+43             keluaranBuff.write(diupperkan); 
+44             keluaranBuff.flush();             
+45             System.out.println("Pesan server yang telah diupperkan sudah terkirim"); 
+46         } 
+47         catch(IOException salah) { 
+48             System.out.println(salah); 
+49         } 
+50         finally { 
+51             if (socket != null) 
+52             socket.close(); 
+53         } 
+54     } 
+55 } 
+import java.net.Socket; 
+2 import java.net.UnknownHostException; 
+3 
+ 
+4 import java.io.IOException; 
+5 import java.io.InputStreamReader; 
+6 import java.io.Reader; 
+7 import java.io.BufferedReader; 
+8 
+ 
+9 import java.io.OutputStreamWriter; 
+10 import java.io.Writer; 
+11 import java.io.BufferedWriter; 
+12 
+ 
+13 import java.util.Scanner; 
+14 
+ 
+15 public class Client { 
+16     public void chat()  
+17                 throws UnknownHostException, IOException { 
+18         Socket socket = new Socket("localhost", 33333); 
+19          
+20         try { 
+21             // Ketik 
+22             Scanner keyboard = new Scanner(System.in); 
+23             System.out.print("Pesan: "); 
+24             String ketikanSatuBaris = keyboard.nextLine(); 
+25                      
+26             // Tulis ke socket 
+27             Writer keluaranWriter = new OutputStreamWriter(socket.getOutputStream());  
+28             BufferedWriter keluaranBuff = new BufferedWriter(keluaranWriter); 
+29             keluaranBuff.write(ketikanSatuBaris); 
+30             keluaranBuff.write("\n"); 
+31             keluaranBuff.flush(); 
+32                  
+33             // Baca dari Server 
+34             System.out.print("Dari server: "); 
+35             Reader masukan = new InputStreamReader(socket.getInputStream());  
+36             BufferedReader masukanBuff = new BufferedReader(masukan); 
+37             String baris = masukanBuff.readLine(); 
+38             System.out.println(baris);              
+39              
+40             // Mengupperkan apa yang dituliskan oleh server 
+41             String diupperkan; 
+42             diupperkan = baris.toUpperCase();             
+43             keluaranBuff.write(diupperkan); 
+44             keluaranBuff.flush();             
+45             System.out.println("Pesan server yang telah diupperkan sudah terkirim"); 
+46         } 
+47         catch(IOException salah) { 
+48             System.out.println(salah); 
+49         } 
+50         finally { 
+51             if (socket != null) 
+52             socket.close(); 
+53         } 
+54     } 
+55 } 
+qweeeeeeee
--- 28-tebak tugas/Client.java
+++ 28-tebak tugas/Client.java
@@ -1,15 +1,12 @@
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.BufferedReader;
-
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.io.BufferedWriter;
-
 import java.util.Scanner;
 
 public class Client {
